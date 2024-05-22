3c13fc2c11267737e24ad4f967f3f1e7c354d03f
Patrick-Wampe <47600274+Patrick-Wampe@users.noreply.github.com>
2024-05-22 14:31:53 +0200

    Create connexion.py

---
+++ streamlit/connexion.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
+import multipage_option_menu
+
+with open('config.yml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+#print(config['credentials'])
+#>>>
+#{'usernames': {'jsmith': {'email': 'jsmith@gmail.com', 'failed_login_attempts': 0, 'logged_in': False, 'name': 'John Smith', 'password': 'abc'}, 'rbriggs': {'email': 'rbriggs@gmail.com', 'failed_login_attempts': 0, 'logged_in': False, 'name': 'Rebecca Briggs', 'password': 'def'}}}
+#{'usernames': {'jsmith': {'email': 'jsmith@gmail.com', 'failed_login_attempts': 0, 'logged_in': False, 'name': 'John Smith', 'password': 'abc'}, 'rbriggs': {'email': 'rbriggs@gmail.com', 'failed_login_attempts': 0, 'logged_in': False, 'name': 'Rebecca Briggs', 'password': 'def'}}}
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+   config['cookie']['expiry_days'],
+    config['pre-authorized']
+)
+
+authenticator.login()
+
+if st.session_state["authentication_status"]:
+    authenticator.logout("Déconnexion")
+    #st.title('Some content')
+    multipage_option_menu.lancement()
+    st.write(f'Welcome *{st.session_state["name"]}*')
+
+elif st.session_state["authentication_status"] is False:
+    st.error('Username/password is incorrect')
+elif st.session_state["authentication_status"] is None:
+    st.warning('Please enter your username and password')
