42dcaeb9fa3db284da4f08caafeef9febcb4c736
Patrick-Wampe <47600274+Patrick-Wampe@users.noreply.github.com>
2024-05-22 14:30:08 +0200

    Update multipage_option_menu.py

--- streamlit/multipage_option_menu.py
+++ streamlit/multipage_option_menu.py
@@ -7,6 +7,7 @@ import photos
 import videos
 import prenom
 
+
 #with st.sidebar:
 #    choix = option_menu(
 #        menu_icon="bi bi-box-seam-fill",
@@ -19,29 +20,30 @@ import prenom
 #with st.sidebar:
 #    st.title("Hello je suis une sidebar !")
 
-choix = option_menu(
-        menu_icon="bi bi-box-seam-fill",
-        menu_title="Menu",
-        options=["Accueil", "Photos", "Vidéos", "Qui es-tu ?"],
-        default_index=0,
-        orientation="horizontal",
-        styles={
-            "icon" : {"color" : "red"},
-        }
-    )
+def lancement():
+    choix = option_menu(
+            menu_icon="bi bi-box-seam-fill",
+            menu_title="Menu",
+            options=["Accueil", "Photos", "Vidéos", "Qui es-tu ?", "Déconnexion"],
+            default_index=0,
+            orientation="horizontal",
+            styles={
+                "icon" : {"color" : "red"},
+            }
+        )
 
-if "prenom" not in st.session_state:
-    st.session_state["prenom"] = ""
-if "photoURL" not in st.session_state:
-    st.session_state["photoURL"] = ""
+    if "prenom" not in st.session_state:
+        st.session_state["prenom"] = ""
+    if "photoURL" not in st.session_state:
+        st.session_state["photoURL"] = ""
 
-if choix == "Accueil":
-    st.title(f"Hello {st.session_state['prenom']} !")
-elif choix == "Photos":
-    #st.title("Mes photos")
-    photos.lancement()
-elif choix == "Vidéos":
-    #st.title("Mes vidéos")
-    videos.lancement()
-elif choix == "Qui es-tu ?":
-    prenom.lancement()
+    if choix == "Accueil":
+        st.title(f"Hello {st.session_state['prenom']} !")
+    elif choix == "Photos":
+        #st.title("Mes photos")
+        photos.lancement()
+    elif choix == "Vidéos":
+        #st.title("Mes vidéos")
+        videos.lancement()
+    elif choix == "Qui es-tu ?":
+        prenom.lancement()
